34c40175bc5d26361aaa6c9bc7ff317d5f75772b
daelvn <daelvn@gmail.com>
2017-05-25 19:09:56 +0200

    Initial commit

---
+++ src/main.lua
@@ -0,0 +1,15 @@
+--[[
+lxpp | Lua Extra Pre-processor
+     | Pantheon Project
+
+Filename
+  main.lua
+Author
+  Cristian Muñiz (daelvn@gmail.com)
+Version
+  lxpp-0.1
+License
+  MIT License
+Description
+  lxpp is a macro pre-processor for Lua, aiming to close cleaner and easier code
+]]--
